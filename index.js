f67c38bee12021315eb724da4313681da9c23ea4
MrBarker47 <jebarker7@gmail.com>
2024-11-05 14:06:12 -0500

    Update files

--- index.js
+++ index.js
@@ -1,15 +1,11 @@
-const key =
-  "live_ZSbsqiS0v6wbxkn52iggIUmWircyjDm5mpYb9DClSh0DvVYz3w9qKgeOw29XsGFG";
-
 //Variables
-
 let img = document.getElementById("img");
 
 //fetch
 const headers = new Headers({
   "Content-Type": "application/json",
   "x-api-key":
-    "live_ZSbsqiS0v6wbxkn52iggIUmWircyjDm5mpYb9DClSh0DvVYz3w9qKgeOw29XsGFG",
+  "live_ZSbsqiS0v6wbxkn52iggIUmWircyjDm5mpYb9DClSh0DvVYz3w9qKgeOw29XsGFG",
 });
 
 let requestOptions = {
@@ -18,17 +14,16 @@ let requestOptions = {
   redirect: "follow",
 };
 
-fetch(
-  "https://api.thedogapi.com/v1/images/search?size=med&mime_types=jpg&format=json&has_breeds=true&order=RANDOM&page=0&limit=1",requestOptions)
+ async function newDog() {
+  fetch("https://api.thedogapi.com/v1/images/search?size=med&mime_types=jpg&format=json&has_breeds=true&order=RANDOM&page=0&limit=1",requestOptions)
   .then((response) => response.text())
   .then((result) => console.log(result))
   .catch((error) => console.log("error", error));
+   
+ }
+ 
+//event listener
+img.addEventListener("click", newDog);
 
 
-
-img.addEventListener("click", (e) => {
-   fetch("https://api.thedogapi.com/v1/images/search?size=med&mime_types=jpg&format=json&has_breeds=true&order=RANDOM&page=0&limit=1",requestOptions)
-  .then((response) => response.text())
-  .then((result) => console.log(result))
-  .catch((error) => console.log("error", error));
-});
+newDog();
