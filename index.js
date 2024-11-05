9ec519dadf60402271e6677007428db9562042e6
MrBarker47 <jebarker7@gmail.com>
2024-11-05 14:44:16 -0500

    update files

--- index.js
+++ index.js
@@ -4,8 +4,7 @@ let img = document.getElementById("img");
 //fetch
 const headers = new Headers({
   "Content-Type": "application/json",
-  "x-api-key":
-  "live_ZSbsqiS0v6wbxkn52iggIUmWircyjDm5mpYb9DClSh0DvVYz3w9qKgeOw29XsGFG",
+  "x-api-key": "live_ZSbsqiS0v6wbxkn52iggIUmWircyjDm5mpYb9DClSh0DvVYz3w9qKgeOw29XsGFG",
 });
 
 let requestOptions = {
@@ -14,16 +13,17 @@ let requestOptions = {
   redirect: "follow",
 };
 
- async function newDog() {
+ 
   fetch("https://api.thedogapi.com/v1/images/search?size=med&mime_types=jpg&format=json&has_breeds=true&order=RANDOM&page=0&limit=1",requestOptions)
   .then((response) => response.text())
   .then((result) => console.log(result))
   .catch((error) => console.log("error", error));
    
- }
+ 
  
 //event listener
-img.addEventListener("click", newDog);
+img.addEventListener("click", (e) => {
+  
+});
 
 
-newDog();
